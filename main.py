612b89ae139ea23962406ab487524c7a71bb288d
Jason Jokumsen <jasonjokumsen177@gmail.com>
2025-05-06 09:07:09 +0200

    use more common english words

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, render_template, flash, session
 import random
 from db import get_db, create_tables
-from words import word_list
+from words import word_list, rand_word
 from wordle import feedback
 
 app = Flask(__name__)
 app.secret_key = '1234'
 
-random_word = random.choice(word_list)
-
+random_word = rand_word(1)[0]
+prev_guess = [""]
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    prev_guess = [""]
+    #prev_guess = [""]
     if request.method == "POST":
         guess = request.form['guess']
         prev_guess.append(guess)
